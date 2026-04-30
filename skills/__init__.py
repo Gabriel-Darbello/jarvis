e7b7be520c0cbd5430b0f718967ec63f0469752f
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-04-29 21:49:46 -0300

    feat: adição da skill base e init

---
+++ skills/__init__.py
@@ -0,0 +1,1 @@
+avaible_skills = {}
---
+++ skills/base.py
@@ -0,0 +1,23 @@
+import os
+
+class SystemCommandSkill:
+    def __init__(self):
+        self.whitelist = ['ls', 'git', 'cat', 'echo', 'mkdir', 'touch', 'cp', 'rm']
+        self.allowed_zones = [
+            os.path.expanduser("~/Área de trabalho"),
+            os.path.expanduser("~/programação/pessoal"),
+        ]
+
+    def _is_safe_path(self, path):
+        if not path:
+            return False
+
+        target_path = os.path.abspath(os.path.expanduser(path))
+        for zone in self.allowed_zones:
+            if target_path.startswith(os.path.abspath(zone)):
+                return True
+
+        return False
+
+    def execute(self, **kwargs):
+        raise NotImplementedError("Você deve implementar o método execute!")
